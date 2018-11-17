a2a2fb4c7f9e2659c87c17a2bbe79a2683eba0ae
Zenith Manuel <zenithmanuel@gmail.com>
2018-11-17 18:41:32 +0800

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+var button = document.querySelector('.button');
+var left = document.querySelector('.flex-left');
+var right = document.querySelector('.flex-right');
+var line1 = document.querySelector('#line1')
+var line2 = document.querySelector('#line2')
+var line3 = document.querySelector('#line3')
+
+
+var toggleOn = false;
+
+button.addEventListener("click", myFunc);
+
+
+function myFunc(){
+    if(toggleOn === false) {
+        left.classList.add('toggleOn');
+        right.classList.add('toggleOn');
+        line1.classList.add('toggleOn');
+        line2.classList.add('toggleOn');
+        line3.classList.add('toggleOn');
+        button.classList.add('toggleOn');
+        toggleOn = true;
+        console.log(toggleOn);
+    } else {
+        left.classList.remove('toggleOn');
+        right.classList.remove('toggleOn');
+        line1.classList.remove('toggleOn');
+        line2.classList.remove('toggleOn');
+        line3.classList.remove('toggleOn');
+        button.classList.remove('toggleOn');
+        toggleOn = false;
+        console.log(toggleOn);
+    }
+
+};
+
