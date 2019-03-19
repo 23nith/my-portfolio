8f5f17a485330a016c2598ec836b1c3b39816975
Zenith Manuel <zenithmanuel@gmail.com>
2019-03-19 20:22:07 +0800

    to add detailed coordinates

--- dist/js/map.js
+++ dist/js/map.js
@@ -1,6 +1,6 @@
 function initMap(){
-  var coord = { lat: 14.6760, lng: 121.0437};
-  var myOptions = {zoom: 13, center: coord};
+  var coord = { lat: 14.6691, lng: 121.0343};
+  var myOptions = {zoom: 16, center: coord};
   var map = new google.maps.Map(document.getElementById('map'), myOptions);
   var marker = new google.maps.Marker({position: coord, map: map});
 }
