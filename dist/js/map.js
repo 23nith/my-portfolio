69fb4c3021bfd691a6737140c60f91d60a2b5106
Zenith Manuel <zenithmanuel@gmail.com>
2019-03-19 20:23:46 +0800

    to zoom in further

--- dist/js/map.js
+++ dist/js/map.js
@@ -1,6 +1,6 @@
 function initMap(){
   var coord = { lat: 14.6691, lng: 121.0343};
-  var myOptions = {zoom: 16, center: coord};
+  var myOptions = {zoom: 18, center: coord};
   var map = new google.maps.Map(document.getElementById('map'), myOptions);
   var marker = new google.maps.Marker({position: coord, map: map});
 }
