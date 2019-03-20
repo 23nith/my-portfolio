3c161eed871ba0cd32ae02d45fb6953b9a0b5ea7
Zenith Manuel <zenithmanuel@gmail.com>
2019-03-20 23:32:21 +0800

    added converter options

--- dist/js/htmltopdf.js
+++ dist/js/htmltopdf.js
@@ -7,8 +7,11 @@ function process(){
   let key = '059bf605ddefad0464c144452ad5218d';
   let theHtml = document.getElementById('userInput').value;
   console.log(theHtml);
+  let size = document.querySelector('#size').value;
+  let orientation = document.querySelector('#orientation').value;
   let url = `http://api.pdflayer.com/api/convert?access_key=${key}&document_url=${theHtml}`;
-
+  url = url + `&page_size=${size}&orientation=${orientation}`; 
+  
   var myInit = { 
                method: 'POST',
                mode: 'cors',
@@ -18,6 +21,7 @@ function process(){
   var myRequest = new Request(url, myInit);
 
   fetch(myRequest).then(function(response) {
+    console.log(url);
     return response.blob();
   }).then(function(myBlob) {
 
@@ -31,6 +35,10 @@ function process(){
     a.download = 'pdflayer.pdf';
     document.getElementById('loading').style.content = "";
     document.getElementById('loading').innerHTML = '<h2>Conversion Done</h2>';
+    setTimeout(function (){
+      document.getElementById('loading').innerHTML = '';
+    }, 5000);
+    
     a.click();
   
     window.URL.revokeObjectURL(url);
