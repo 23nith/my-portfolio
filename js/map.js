63e19d973819f03322e13186e332b33191d98691
Zenith Manuel <user.name>
2021-10-20 01:00:50 +0800

    Updates

--- js/map.js
+++ js/map.js
@@ -2,5 +2,5 @@ function initMap(){
   var coord = { lat: 14.6691, lng: 121.0343};
   var myOptions = {zoom: 18, center: coord, mapTypeId: google.maps.MapTypeId.HYBRID};
   var map = new google.maps.Map(document.getElementById('map'), myOptions);
-  var marker = new google.maps.Marker({position: coord, map: map});
+  var marker = new google.maps.Marker({position: coord, map: map, title: 'I live here'});
 }
