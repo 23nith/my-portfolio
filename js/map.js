844e6109b54aacc05b56e46811930bc9e2640ef2
23nith <user.name>
2022-10-25 19:51:02 +0800

    Updates

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function initMap(){
   var coord = { lat: 14.6691, lng: 121.0343};
-  var myOptions = {zoom: 18, center: coord, mapTypeId: google.maps.MapTypeId.HYBRID};
+  var myOptions = {zoom: 18, center: coord};
   var map = new google.maps.Map(document.getElementById('map'), myOptions);
-  var marker = new google.maps.Marker({position: coord, map: map, title: 'I live here'});
+  var marker = new google.maps.Marker({position: coord, map: map});
 }
